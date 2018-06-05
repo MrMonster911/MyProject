ac660f82a12ed8b8ba149d38f8475e08f2577681
MrMonster911 <mr.monster@gmail.com>
2018-06-05 15:12:17 +0200

    Tried to use channels and aded some external data to test some I/O functions.

--- HelloWorld.go
+++ HelloWorld.go
@@ -1,7 +1,59 @@
 package main
 
-import "fmt"
+import (
+	"bufio"
+	"fmt"
+	"log"
+	"os"
+    "time"
+    "net/http"
+)
 
-func main() {  
-    fmt.Println("Hello Friggin' World")
+type Event struct {
+	Hest string
+}
+
+func main() {
+	fmt.Println("Hello Friggin' World")
+	ch := make(chan Event, 1000)
+	//  ch <-
+	go populateChan(ch)
+    	
+    // time.Sleep(1 * time.Second)
+    go process(ch)
+
+        go process(ch)
+    go process(ch)
+    go process(ch)
+
+    http.HandleFunc("/", func (w http.ResponseWriter, r *http.Request) {
+	   fmt.Fprint(w, "Welcome to my website!")
+        ch <- Event{Hest: "request"}
+    })
+    http.ListenAndServe(":8080", nil)
+}
+
+func populateChan(extdata chan Event) {
+	file, err := os.Open("data")
+	if err != nil {
+		log.Fatal(err)
+	}
+	defer file.Close()
+	scanner := bufio.NewScanner(file)
+	for scanner.Scan() {
+        time.Sleep(1 * time.Second)
+        e := Event{Hest: scanner.Text()}
+		extdata <- e
+	}
+    
+}
+
+func process(c chan Event) {
+	fmt.Println("Processing...")
+    i := 0
+    for e := range(c){
+        i = i + 1
+        fmt.Printf("Godt event (%+v): %+v\n", i, &e)
+        time.Sleep(1 * time.Second)
+	}
 }
